15c27722e01e1ccd88cc1c3ed5ec477b5a4b05d7
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2022-01-03 18:53:43 +0400

    first deplo

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { mathGen } from 'samathgen';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/gen', (req, res) => {
+  const length = req.query.length;
+  const answer = req.query.answer;
+  const brackets = req.query.brackets;
+  const options = req.query.options;
+  if (!length) res.error("error");
+  res.send(mathGen(length, {
+    "answer": answer,
+    "brackets": brackets,
+    "quizOptions": options
+  }))
+})
+
+app.listen(port)
+console.log(`http://localhost:${port}`)
